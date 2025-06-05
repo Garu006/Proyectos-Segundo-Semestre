0a196e057e9028b9526b64b36cb07dea9e3979f0
Garu006 <garojas@uamv.edu.ni>
2025-06-04 19:51:42 -0600

    Ejercicios de grafos especiales

---
+++ Semana11/ejercicio3.py
@@ -0,0 +1,24 @@
+'''Crear y mostrar un grafo en profundidad'''
+
+def dfs(grafo, nodo, visitado=None):
+    if visitado is None:
+        visitado = set()
+    visitado.add(nodo)
+    print(nodo, end=' ')
+    for vecino in grafo[nodo]:
+        if vecino not in visitado:
+            dfs(grafo, vecino, visitado)
+
+grafo = {
+    "gabo": ["joha", "niko", "val"],
+    "joha": ["gabo", "niko", "val"],
+    "niko": ["gabo", "joha", "val"],
+    "val": ["gabo", "joha", "niko"]
+}
+
+print("Conexiones del grafo:")
+for nodo in grafo:
+    print(f"{nodo} conectado con: {grafo[nodo]}")
+
+print("\nRecorrido en profundidad (DFS):")
+dfs(grafo, "gabo")
